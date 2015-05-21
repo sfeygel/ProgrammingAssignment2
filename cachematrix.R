766f9169b6c8e04bf4732fd20f006b9770209c64
sfeygel <sfeygel@yahoo.com>
2015-05-21 18:34:45 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,3 +1,12 @@
+<<<<<<< HEAD
+
+
+## makeCacheMatrix function creates a special "matrix" object that can cache the input matrix and its inverse
+
+## cacheSolve function calls functions stored in the special "matrix" returned by makeCacheMatrix
+
+
+
 makeCacheMatrix <- function(x = matrix()) {
   m<-NULL
   set<-function(y){
@@ -12,6 +21,22 @@ list(set=set, get=get,
    getmatrix=getmatrix)
 }
 
+=======
+makeCacheMatrix <- function(x = matrix()) {
+  m<-NULL
+  set<-function(y){
+  x<<-y
+  m<<-NULL
+}
+get<-function() x
+setmatrix<-function(solve) m<<- solve
+getmatrix<-function() m
+list(set=set, get=get,
+   setmatrix=setmatrix,
+   getmatrix=getmatrix)
+}
+
+>>>>>>> ded0b86be7645a7d99958c99477b26a4c93a1121
 cacheSolve <- function(x=matrix(), ...) {
     m<-x$getmatrix()
     if(!is.null(m)){
